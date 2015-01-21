97e3eecc05f870550326247d4598441167fbd6fa
Cardaccia Roberto <robcardaccia22@gmail.com>
2015-01-21 09:17:18 +0100

    ciao

--- main.c
+++ main.c
@@ -1,46 +1,122 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 const int N=5; // dimensione massima dell'array
 
+
+
 int massimo(int valori[], int dimensione);
+
 int minimo(int valori[], int dimensione);
+
 float media(int valori[], int dimensione);
 
+
+
 int main(int argc, char** argv) {
+
     int i;
+
     int numeri[N];
-    int max, min, med;
+
+    int max, min;
+
+    float med;
+
     
+
     printf("Inserisci %d valori interi\n", N);
+
     for(i=0; i<N; i++) {
+
         printf("Inserisci il valore di indice %d:", i);
+
         scanf("%d", &(numeri[i]));
+
     }
+
     
+
     max = massimo(numeri, N);
+
     min = minimo(numeri, N);
+
     med = media(numeri, N);
+
     
+
     printf("Il valore MASSIMO e' %d.\n", max);
+
     printf("Il valore MINIMO e' %d.\n", min);
-    printf("Il valore MEDIO e' %d.\n", med);
+
+    printf("Il valore MEDIO e' %f.\n", med);
+
     
+
     return (EXIT_SUCCESS);
+
 }
+
+
 
 int massimo(int valori[], int dimensione){
-    // TODO Implementare il corpo della funzione
-    return 0;
+
+    int i, max = valori[0];
+
+
+
+    for(i=1; i<dimensione; i++)
+
+        if (valori[i]>max)
+
+            max=valori[i];
+
+    
+
+    return max;
+
 }
+
+
 
 int minimo(int valori[], int dimensione){
-    // TODO Implementare il corpo della funzione
-    return 0;
+
+    int i, min=valori[0];
+
+
+
+    for(i=1; i<dimensione; i++)
+
+        if (valori[i]<min)
+
+            min=valori[i];
+
+    
+
+    return min;
+
 }
+
+
 
 float media(int valori[], int dimensione){
-    // TODO Implementare il corpo della funzione
-    return 0;
-}
 
+    int i;
+
+    float media=0;
+
+    
+
+    for(i=0; i<dimensione; i++)
+
+        media+=valori[i];
+
+        media = media/(float)dimensione;
+
+    
+
+    return media;
+
+}
--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
-CND_ARTIFACT_NAME_Debug=ese003-funzionisuarray
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/ese003-funzionisuarray
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
-CND_PACKAGE_NAME_Debug=ese003-funzionisuarray.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/ese003-funzionisuarray.tar
+CND_PLATFORM_Debug=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_ARTIFACT_NAME_Debug=ese003
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/ese003
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_PACKAGE_NAME_Debug=ese003.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/ese003.tar
 # Release configuration
-CND_PLATFORM_Release=Cygwin-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
-CND_ARTIFACT_NAME_Release=ese003-funzionisuarray
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/ese003-funzionisuarray
-CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
-CND_PACKAGE_NAME_Release=ese003-funzionisuarray.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/ese003-funzionisuarray.tar
+CND_PLATFORM_Release=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
+CND_ARTIFACT_NAME_Release=ese003
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/ese003
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
+CND_PACKAGE_NAME_Release=ese003.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/ese003.tar
 #
 # include compiler specific variables
 #
